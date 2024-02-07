6f6c4dcbaa9c7cd730223d2d09d91fea390b5b9e
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2024-02-07 22:43:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function aveSpd(upTime, upSpd, downSpd) {
   // your code here
+	let distanceUp=(upSpd*(upTime/60));
+	let downTime=distanceUp/downSpd;
+	return((2*distanceUp)/((upTime/60)+downTime));
 }
 
 // Do not change the code below
@@ -7,4 +10,4 @@ const upTime = prompt("Enter upTime: ");
 const downTime = prompt("Enter downTime: ");
 const downSpd = prompt("Enter downSpd: ");
 
-alert(aveSpd(upTime, downTime, downSpd));
+alert(aveSpd(upTime, downTime, downSpd))
